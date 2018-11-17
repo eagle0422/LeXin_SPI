41395e37c732d583258ae6283eefb579ee6d1de4
20p153 <eagle-lee@qq.com>
2018-11-17 11:26:30 +0800

    spi exaple

---
+++ Output/spi_adc.d
@@ -0,0 +1,14 @@
+..\output\spi_adc.o: SPI_ADC.c
+..\output\spi_adc.o: ..\FWlib\inc\stm32f10x_spi.h
+..\output\spi_adc.o: ..\CMSIS\stm32f10x.h
+..\output\spi_adc.o: ..\CMSIS\core_cm3.h
+..\output\spi_adc.o: d:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+..\output\spi_adc.o: ..\CMSIS\system_stm32f10x.h
+..\output\spi_adc.o: ..\USER\stm32f10x_conf.h
+..\output\spi_adc.o: ..\FWlib\inc\stm32f10x_adc.h
+..\output\spi_adc.o: ..\CMSIS\stm32f10x.h
+..\output\spi_adc.o: ..\FWlib\inc\stm32f10x_dma.h
+..\output\spi_adc.o: ..\FWlib\inc\stm32f10x_flash.h
+..\output\spi_adc.o: ..\FWlib\inc\stm32f10x_gpio.h
+..\output\spi_adc.o: ..\FWlib\inc\stm32f10x_rcc.h
+..\output\spi_adc.o: ..\FWlib\inc\stm32f10x_usart.h
